16ab65e6b456360619147b813d4516337f6d91be
manmohanbatra <53882458+manmohanbatra@users.noreply.github.com>
2020-12-14 12:52:55 -0700

    Update provider.tf

--- provider.tf
+++ provider.tf
@@ -6,10 +6,19 @@ resource "azurerm_resource_group" "myterraformgroup" {
         environment = "Terraform Demo"
     }
    
+    # Configure the Azure provider
+provider "azurerm" { 
+  # The "feature" block is required for AzureRM provider 2.x. 
+  # If you are using version 1.x, the "features" block is not allowed.
+  version = "~>2.0"
+  features {}
+}
+terraform {
     backend "azurerm" {
     resource_group_name   = "tstate"
     storage_account_name  = "tstate09762"
     container_name        = "tstate"
     key                   = "terraform.tfstate"
   }
+}
 }
