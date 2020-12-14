456b56d899e8b85dfa958c1448f0d73289e7ed50
manmohanbatra <53882458+manmohanbatra@users.noreply.github.com>
2020-12-14 13:24:32 -0700

    Update provider.tf

--- provider.tf
+++ provider.tf
@@ -1,24 +1,24 @@
-resource "azurerm_resource_group" "myterraformgroup" {
-    name     = "myResourceGroup"
-    location = "westus"
+terraform {
+  backend "azurerm" {
+    resource_group_name   = "tstate"
+    storage_account_name  = "tstate126"
+    container_name        = "tstate"
+    key                   = " 7iSYsDRv83HyhEg90myOdVG56CkVPxBd4gnFOELXrPebuYtdHi8fqOWGrGQcR17jTWzEMAvlDpBKdnMWFI1kBg=="
+  }
+}
 
-    tags = {
-        environment = "Terraform Demo"
-    }
-   
-    # Configure the Azure provider
-provider "azurerm" { 
-  # The "feature" block is required for AzureRM provider 2.x. 
+# Configure the Azure provider
+provider "azurerm" {
+  # The "feature" block is required for AzureRM provider 2.x.
   # If you are using version 1.x, the "features" block is not allowed.
   version = "~>2.0"
   features {}
 }
-terraform {
-    backend "azurerm" {
-    resource_group_name   = "tstate"
-    storage_account_name  = "tstate09762"
-    container_name        = "tstate"
-    key                   = "terraform.tfstate"
-  }
-}
+
+resource "azurerm_resource_group" "state-demo-secure" {
+  name     = "state-demo"
+  location = "westus"
+  tags = {
+        environment = "Terraform Demo"
+    }
 }
