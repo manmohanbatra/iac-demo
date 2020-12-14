9c7861c57e2f18bbce6eebd4faaa5c1dbc2ea50e
manmohanbatra <53882458+manmohanbatra@users.noreply.github.com>
2020-12-14 12:40:36 -0700

    Update vpc.tf

--- vpc.tf
+++ vpc.tf
@@ -1,9 +1,10 @@
-resource "aws_vpc" "main" {
-  cidr_block       = "10.0.0.0/16"
-  instance_tenancy = "default"
+resource "azurerm_virtual_network" "myterraformnetwork" {
+    name                = "myVnet"
+    address_space       = ["10.0.0.0/16"]
+    location            = "westus"
+    resource_group_name = azurerm_resource_group.myterraformgroup.name
 
-  tags = {
-    Name = "demo-vpc"
-    Purpose = "Jenkins Demo"
-  }
+    tags = {
+        environment = "Terraform Demo"
+    }
 }
